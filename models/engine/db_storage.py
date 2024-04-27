e409f652a8ee074da4f6bb9e753a469ebeb2f453
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-04-27 15:05:18 +0100

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -19,9 +19,8 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 classes = {"Amenity": Amenity, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
-
 class DBStorage:
-    """interaacts with the MySQL database"""
+    """Interacts with the MySQL database"""
     __engine = None
     __session = None
 
@@ -41,7 +40,7 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """query on the current database session"""
+        """Query on the current database session"""
         new_dict = {}
         for clss in classes:
             if cls is None or cls is classes[clss] or cls is clss:
@@ -49,28 +48,60 @@ class DBStorage:
                 for obj in objs:
                     key = obj.__class__.__name__ + '.' + obj.id
                     new_dict[key] = obj
-        return (new_dict)
+        return new_dict
 
     def new(self, obj):
-        """add the object to the current database session"""
+        """Add the object to the current database session"""
         self.__session.add(obj)
 
     def save(self):
-        """commit all changes of the current database session"""
+        """Commit all changes of the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete from the current database session obj if not None"""
+        """Delete from the current database session obj if not None"""
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """reloads data from the database"""
+        """Reloads data from the database"""
         Base.metadata.create_all(self.__engine)
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
         self.__session = Session
 
     def close(self):
-        """call remove() method on the private session attribute"""
+        """Call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """
+        Retrieves an object based on the class and its ID.
+
+        Args:
+            cls (str): The class of the object to retrieve.
+            id (str): The ID of the object to retrieve.
+
+        Returns:
+            obj or None: The object matching the class and ID, or None if not found.
+        """
+        if cls is None or id is None or not isinstance(cls, str) or not isinstance(id, str):
+            return None
+
+        obj = self.all(cls).get(id)
+        return obj
+
+    def count(self, cls=None):
+        """
+        Counts the number of objects in storage matching the given class.
+
+        Args:
+            cls (str, optional): The class to count objects for. If None, counts all objects.
+
+        Returns:
+            int: The number of objects matching the given class.
+        """
+        if cls is None:
+            return len(self.all())
+        else:
+            return len(self.all(cls))
