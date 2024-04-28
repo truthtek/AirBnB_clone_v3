512197041078efc85c6f0fbcb06a0546817692d2
truthtek <adedokunadebayolukman@gmail.com>
2024-04-28 14:50:02 +0000

    error fixed

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python3
+"""create blueprint"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-from api.v1.views.index import *
+
+if app_views is not None:
+    from api.v1.views.index import *
