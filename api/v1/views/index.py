df282d2e7b8d253ee2e1ccb9116f6c27902cb1cf
truthtek <adedokunadebayolukman@gmail.com>
2024-04-28 15:02:32 +0000

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,19 @@
-from api.v1.views import app_views
+from models import storage
 from flask import jsonify
+from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'])
 def status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def object_stats():
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
